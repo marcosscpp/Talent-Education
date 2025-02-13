e3d5016dfaf5b5d7c29d9193523bea5a7d662535
Marcos <marcosscpp@gmail.com>
2025-02-13 16:10:50 -0300

    Removing Modal Registration form, and inserting Registration Form Section

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-import Modal from "./modules/modal";
 import Form from "./modules/form";
 import Faq from "./modules/faq";
 import triggerEvent from "./modules/triggerEvent";
@@ -12,10 +11,5 @@ Aos.init({
   once: true,
 });
 
-const modal = new Modal(
-  "[data-modal='open']",
-  "[data-modal='close']",
-  "[data-modal='container']"
-).init();
 const form = new Form("[data-form]");
 const faq = new Faq("[data-faq] dt").init();
